7fdf5cb9824622203d30ec47f61ef13f5b74d5f5
rekasys <vytasrkss@gmail.com>
2018-12-07 23:47:49 +0200

    Create process.php

---
+++ process.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($_POST['submit'])) {
+$to = "innorbutai@gmail.com";
+$subject = $_POST['subject'];
+$txt = $_POST['message'];
+$email = $_POST['email'];
+$headers = "Form:" .$email "\r\n" .
+"CC: vytasrkss@gmail.com";
+
+mail($to,$subject,$txt,$headers);
+header("Location: contact.html");
+
+
+}
+
+
+?>
