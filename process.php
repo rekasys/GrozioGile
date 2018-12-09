9e43a501e443b52cacb524c7a1531ee4ddf63f7f
rekasys <vytasrkss@gmail.com>
2018-12-09 23:17:49 +0200

    Update process.php

--- process.php
+++ process.php
@@ -1,14 +1,6 @@
 <?php
-//*if (isset($_POST['submit'])) {
-$to = "innorbutai@gmail.com";
-$subject = $_POST['subject'];
-$txt = $_POST['message'];
-$email = $_POST['email'];
-$headers = "Form:" .$email "\r\n" .
-"CC: vytasrkss@gmail.com";
+if (isset($_POST['submit'])) {
 
-mail($to,$subject,$txt,$headers);
-header("Location: contact.html");*//
 echo "it works";
 
 }
